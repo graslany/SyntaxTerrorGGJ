8b23716983223ef97198f08879e6134d18433ec2
Chris Jungen <chris.jungen@epitech.eu>
2018-01-27 16:59:10 +0100

    Ajout du levier

---
+++ Unity/Assets/LevierBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevierBehaviour : MonoBehaviour {
+    bool _Activated;
+    Animator _Animator;
+	// Use this for initialization
+	void Start () {
+        _Activated = false;
+        _Animator = GetComponent<Animator>();
+    }
+
+    void PlayLeverAnimation(bool reverse = false)
+    {
+        if (reverse)
+        {
+            _Animator.Play("LevierDisabled");
+        }
+        else
+        {
+            _Animator.Play("LevierActivated");
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            PlayLeverAnimation(_Activated);
+            _Activated = !_Activated;
+            Debug.Log(_Activated);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
