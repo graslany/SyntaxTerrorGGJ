c53c370da8e4085700831d4f366e90212f4e499d
Yves Grasland <yves.grasland@gmx.fr>
2018-01-27 06:00:11 +0100

    Beware ! Voici le proof-of-concept le plus crados du monde de transmission d'infos entre joueurs :-)

---
+++ Unity/Assets/Levels/Yves test/PlayerObject.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerObject : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnSecene1ColliderEnter() {
+		CmdScene1ColliderEnter ();
+	}
+
+	public void OnSecene1ColliderExit() {
+		CmdScene1ColliderExited ();
+	}
+
+	[Command]
+	private void CmdScene1ColliderEnter() {
+		RpcScene1ColliderEnter ();
+	}
+
+	[Command]
+	private void CmdScene1ColliderExited() {
+		RpcScene1ColliderExited ();
+	}
+
+	[ClientRpc]
+	public void RpcScene1ColliderEnter() {
+		Player2Display p2 = GameObject.FindObjectOfType<Player2Display> ();
+		if (p2 != null)
+			p2.Player1EnteredZone ();
+	}
+
+	[ClientRpc]
+	public void RpcScene1ColliderExited() {
+		Player2Display p2 = GameObject.FindObjectOfType<Player2Display> ();
+		if (p2 != null)
+			p2.Player1ExitedZone ();
+	}
+}
---
+++ Unity/Assets/Levels/Yves test/StartButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour {
+
+	public NetworkManager nwManager;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnGUI() {
+		if (GUI.Button (new Rect (10, 70, 50, 30), "Start server")) {
+			Debug.Log ("Server start");
+
+			nwManager.StartHost ();
+			SceneManager.LoadScene ("Room 1");
+		}
+
+		if (GUI.Button (new Rect (50, 70, 50, 30), "Join")) {
+			Debug.Log ("Client start");
+			nwManager.StartClient ();
+			SceneManager.LoadScene ("Room 2");
+		}
+
+	}
+}
