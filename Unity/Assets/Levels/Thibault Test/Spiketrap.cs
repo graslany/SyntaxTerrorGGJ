9efd22fc47a7feab86cb547eccb8cb5d8bc6064c
Unknown <hypoter@hotmail.fr>
2018-01-27 20:16:05 +0100

    Piege pique

---
+++ Unity/Assets/Levels/Thibault Test/Spiketrap.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spiketrap : MonoBehaviour,TrapInterface {
+    [SerializeField] bool _IsActivated;
+    public GameObject _spritePrefab;
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Trigger();
+    }
+
+    public void Trigger()
+    {
+        Debug.Log("trig");
+        if (_IsActivated)
+        {
+            Spring();
+        }
+    }
+
+    public void Spring()
+    {
+        print("tets");
+        _spritePrefab.transform.Translate(new Vector3(0, 0.5f, 0));
+    }
+
+    // Update is called once per frame
+    public void Deactivate()
+    {
+        _IsActivated = false;
+    }
+
+    public void Reactivate()
+    {
+        _IsActivated = true;
+    }
+
+}
