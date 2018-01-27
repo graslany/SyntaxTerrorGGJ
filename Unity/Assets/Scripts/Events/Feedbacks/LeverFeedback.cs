7498e220fe3255d655b72076540c3598ecbbf3c8
Yves Grasland <yves.grasland@gmx.fr>
2018-01-27 19:29:43 +0100

    Travaux sur les leviers

---
+++ Unity/Assets/Scripts/Events/Feedbacks/LeverFeedback.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverFeedback : MonoBehaviour, IValuesUser<bool> {
+
+	public string variableName;
+	private Animator _animator;
+
+	void Start () {
+        _animator = GetComponent<Animator>();
+		if (_animator != null)
+			Debug.LogError ("Impossible de trouver l'animateur cible à piloter");
+    }
+
+	public void OnValueChanged(string variableName, bool newValue) {
+		if (_animator != null) {
+			if (newValue)
+				_animator.Play("LevierDisabled");
+			else
+				_animator.Play("LevierActivated");
+		}
+	}
+}
