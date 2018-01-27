4f10de7b6c2894a3a5bab3145e5921ca1b5e0587
Yves Grasland <yves.grasland@gmx.fr>
2018-01-27 12:03:44 +0100

    Ajouts de types supplémentaires

--- Unity/Assets/Scripts/Events/IEventReceiver.cs
+++ Unity/Assets/Scripts/Events/IEventReceiver.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
 
 public interface IEventReceiver
 {
---
+++ Unity/Assets/Scripts/Events/Value sources/IntValueSourceMB.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+/// <summary>
+/// Classe qui réceptionne la valeur d'une variable du jeu.
+/// </summary>
+public class IntValueSourceMB : MonoBehaviour {
+
+	[SerializeField]
+	private IntValueSource variable;
+	public IntValueSource Variable {
+		get {
+			return variable;
+		}
+	}
+
+	public IntValueSourceMB() {
+		variable = (variable ?? new IntValueSource ());
+	}
+}
